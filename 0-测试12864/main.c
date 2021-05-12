5f43c1d569b8a22cf68c0dac36b28689a20c5db2
escapefreeg <escapefreeg@email.com>
2021-05-12 16:27:19 +0800

    再次尝试0-测试12864

--- 0-测试12864/main.c
+++ 0-测试12864/main.c
@@ -1,23 +1,22 @@
 #include <REGX52.H>
 #include "LCD12864.h"
 
+//时序问题
+//测试奇数开头的功能是否正常
+
 sbit LCD_PSB =  P3^7;           
-sbit wela    =  P2^6;
-sbit dula    =  P2^7;
+
 
 void main(void){
-    unsigned char key = 0;
 	//init
-    
-    LCD12864_Init();
+
     LCD_PSB = 1;
-    wela=0;
-	dula=0;         
+    LCD12864_Init();
+    
     //
     while(1){
-        //key = MatrixKey();
         LCD12864_ShowNum(1,1,57,2);
-        LCD12864_ShowString(2,2,"hello world");
+        LCD12864_ShowString(2,2,"zello world");
         LCD12864_ShowHexNum(3,2,48,3);
     }
 }
