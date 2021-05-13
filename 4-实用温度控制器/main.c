2174dca9d861e7cc6136950f1c6f520f0ab1cb47
escapefreeg <escapefreeg@email.com>
2021-05-13 22:33:18 +0800

    时间s2019基本功能

--- 4-实用温度控制器/main.c
+++ 4-实用温度控制器/main.c
@@ -1,4 +1,6 @@
 //采用集成版板子开发
+//todo list
+//温度设置上下限判断
 #include <REGX52.H>
 #include "LCD12864.h"
 #include "MatrixKey.h"
@@ -7,10 +9,10 @@
 #include "AT24C02.h"
 #include "DS18B20.h"
 
-bit key1 = 0;
-bit key2 = 0;
-bit key3 = 0;
-bit key4 = 0;
+bit key1 = 0;bit nowkey1 = 0;
+bit key2 = 0;bit nowkey2 = 0;
+bit key3 = 0;bit nowkey3 = 0;
+bit key4 = 0;bit nowkey4 = 0;
 unsigned char key;
 unsigned char THSet;
 unsigned char TLSet;
@@ -48,40 +50,37 @@ void main(void){
     //init
     LCD12864_Init();
     DS18B20_ConvertT();
-    Delayxms(5);
-    TH = 0;
-    TL = 100;
+    Delayxms(1000);
+    ((unsigned char *)(&TH))[0] = AT24C02_ReadByte(1);
+    ((unsigned char *)(&TH))[1] = AT24C02_ReadByte(2);
+    ((unsigned char *)(&TH))[2] = AT24C02_ReadByte(3);
+    ((unsigned char *)(&TH))[3] = AT24C02_ReadByte(4);
+    
+    ((unsigned char *)(&TL))[0] = AT24C02_ReadByte(5);
+    ((unsigned char *)(&TL))[1] = AT24C02_ReadByte(6);
+    ((unsigned char *)(&TL))[2] = AT24C02_ReadByte(7);
+    ((unsigned char *)(&TL))[3] = AT24C02_ReadByte(8);
         //显示摄氏度的基础信息
     LCD12864_ShowString(1,1,"S:");
 
     while (1)
     {
-        DS18B20_ConvertT();
-        TheT = DS18B20_ReadT();
-        ShowT(1,3,TheT);
-        if(TheT < TL){
-            TL = TheT;
-        }
-        if(TheT > TH){
-            TH = TheT;
-        }
         key = MatrixKey();
         //温度转换
         if(key == 1){
-            key1 = !key1;
+            //实现状态位的更改
+            key1 = nowkey1;nowkey1 = !nowkey1;
         }
         //显示最高温度
         else if(key == 2){
-            
-            key2 = !key2;
+            key2 = nowkey2;nowkey2 = !nowkey2;
         }
         //显示最低温度
         else if(key == 3){
-            
-            key3 = !key3;
+            key3 = nowkey3;nowkey3 = !nowkey3;
         }
         else if(key == 4){
-            key4 = !key4;
+            key4 = nowkey4;nowkey4 = !nowkey4;
         }
         else if(key == 5){
             //h ins
@@ -99,49 +98,78 @@ void main(void){
             //l des
             TLSet--;
         }
-
-        //按下显示 和消失
-        if(key1){
-            //11个空格
-            LCD12864_ShowString(2,1,"           ");
+        
+        DS18B20_ConvertT();
+        TheT = DS18B20_ReadT();
+        ShowT(1,3,TheT);
+        if(TheT < TL){
+            TL = TheT;
+            AT24C02_WriteByte(5,((unsigned char *)(&TL))[0]);
+            Delayxms(5);
+            AT24C02_WriteByte(6,((unsigned char *)(&TL))[1]);
+            Delayxms(5);
+            AT24C02_WriteByte(7,((unsigned char *)(&TL))[2]);
+            Delayxms(5);
+            AT24C02_WriteByte(8,((unsigned char *)(&TL))[3]);
+            Delayxms(5);
         }
-        else{
-            LCD12864_ShowString(2,1,"H:");
-            TheTC = TheT*1.8+32;
-            ShowT(2,3,TheTC);
+        if(TheT > TH){
+            TH = TheT;
+            //存储
+            AT24C02_WriteByte(1,((unsigned char *)(&TH))[0]);
+            Delayxms(5);
+            AT24C02_WriteByte(2,((unsigned char *)(&TH))[1]);
+            Delayxms(5);
+            AT24C02_WriteByte(3,((unsigned char *)(&TH))[2]);
+            Delayxms(5);
+            AT24C02_WriteByte(4,((unsigned char *)(&TH))[3]);
+            Delayxms(5);
         }
-        //按下显示 和消失
-        if(key2){
-            LCD12864_ShowString(3,1,"            ");
+        //刷新华氏度
+        if((key1|nowkey1)){
+            if(nowkey1 == 0){
+                LCD12864_ShowString(2,1,"           ");
+            }
+            else{
+                LCD12864_ShowString(2,1,"H:");
+                TheTC = TheT*1.8+32;
+                ShowT(2,3,TheTC);
+            }
         }
-        else{
-            LCD12864_ShowString(3,1,"TH:");
-            ShowT(3,4,TH);
+        if((key2|nowkey2)){
+            if(nowkey2 == 0){
+                LCD12864_ShowString(3,1,"            ");
+            }
+            else{
+                LCD12864_ShowString(3,1,"TH:");
+                ShowT(3,4,TH);
+            }
         }
-        //按下显示 和消失
-        if(key3){
-            LCD12864_ShowString(4,1,"            ");
+        if((key3|nowkey3)){
+            if(nowkey3 == 0){
+                LCD12864_ShowString(4,1,"            ");
+            }
+            else{
+                LCD12864_ShowString(4,1,"TL:");
+                ShowT(4,4,TL);
+            }
         }
-        else{
-            LCD12864_ShowString(4,1,"TL:");
-            ShowT(4,4,TL);
+        if((key4|nowkey4)){
+            if(nowkey4 == 0){
+                LCD12864_ShowString(1,14,"  ");
+                LCD12864_ShowString(2,13,"   ");
+                LCD12864_ShowString(3,14,"  ");
+                LCD12864_ShowString(4,13,"   ");
+            }
+            else{
+                LCD12864_ShowString(1,14,"HN");
+                LCD12864_ShowChar(2,13,'+');
+                LCD12864_ShowNum(2,14,THSet,2);
+                LCD12864_ShowString(3,14,"LN");
+                LCD12864_ShowChar(4,13,'+');
+                LCD12864_ShowNum(4,14,TLSet,2);
+            }
         }
-        //显示和消失设定的最大最小
-        if(key4){
-            LCD12864_ShowString(1,14,"  ");
-            LCD12864_ShowChar(2,13,"   ");
-            LCD12864_ShowString(3,14,"  ");
-            LCD12864_ShowChar(4,13,"   ");
-        }
-        else{
-            LCD12864_ShowString(1,14,"HN");
-            LCD12864_ShowChar(2,13,'+');
-            LCD12864_ShowNum(2,14,THSet);
-            LCD12864_ShowString(3,14,"LN");
-            LCD12864_ShowChar(4,13,'+');
-            LCD12864_ShowNum(4,14,TLSet);
-        }
-
     }
     
 }
