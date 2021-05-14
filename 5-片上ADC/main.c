353650863ae3bb42c91e4f570f5badfe9e190d48
escapefreeg <escapefreeg@email.com>
2021-05-14 15:38:24 +0800

    稍微修改下ADC

--- 5-片上ADC/main.c
+++ 5-片上ADC/main.c
@@ -1,12 +1,12 @@
 #include <REGX52.H>
 #include "LCD12864.h"
 #include "Delay.h"
+
 sfr P1ASF = 0x9D;
 sfr ADC_CONTR = 0xBC;
 sfr ADC_RES = 0xBD;
 sfr ADC_RESL = 0xBE;
 sfr AUXR1 = 0xA2;
-
 
 unsigned char re = 0;
 void main(void){
@@ -16,16 +16,16 @@ void main(void){
 
     //选择ADC引脚
     P1ASF = 0x04;
-    //上电
+    //上电、选择速度、选择通道、初始化标志和开始位
     //0b1110 0010
     ADC_CONTR = 0xE2;
     //延时
     Delayxms(5);
-    //输出格式 ADRJ = 1
+    //输出格式 ADRJ = 0
     AUXR1 &= 0xFB;
-    //打开中断
+    //打开中断，没有位寻址的方式，因为ADC的位置不可以
     IE |= 0xA0; 
-    //启动
+    //启动，修改ADC_CONTR的启动位
     //0b1110 1010
     ADC_CONTR = 0xEA;
 
@@ -43,6 +43,7 @@ void adc () interrupt 5{
     re = ADC_RES;
     //软件清0
     //0b1110 1010
+    //采用直接赋值的方法
     ADC_CONTR = 0xEA;
 }
 
