12d9371692c0fc1f1104a78be739028d717e31ac
escapefreeg <escapefreeg@email.com>
2021-06-12 14:46:59 +0800

    汇编

--- 汇编/main.c
+++ 汇编/main.c
@@ -10,34 +10,43 @@ void main(void)
 	
 	
 	#pragma asm
-		;假设50H和A中已存入数据
-		MOV 51H,#0    ;51H清0,为了放入进位信息
-		ADD A,50H
-		MOV 50H,A 
-		JC HaveC
-		SJMP One
-HaveC:	MOV 51H,#1
-One:	MOV A,#0	  ;结束占位，没有其它意思
+	MOV A,#57H
+	MOV R0,#63H
+	MOV 63H,#0A1H
+	ANL	A,#63H	;A=43H
+	ORL	63H,A		;A=0E3H
+	XRL	A,@R0		;A=42H
+	CPL	A			;A=0BDH
+	MOV result,A
 	#pragma endasm
 
-	LCD12864_ShowNum(1,3,result,3);
-	LCD12864_ShowNum(2,3,result1,3);
+
+	LCD12864_ShowHexNum(1,3,result1,3);
+	LCD12864_ShowHexNum(2,3,result,3);
+	
 	while (1)
 	{
 		;
 	}
 	
-// 	#pragma asm
-// 		MOV 51H,#0    ;51H清0,为了放入进位信息
-// 		MOV 50H,#256
-// 		MOV A,#5
-// 		ADD A,50H
-// 		MOV 50H,A
-// 		MOV result,A 
-// 		JC HaveC
-// 		SJMP One
-// HaveC:	MOV 51H,#1
-// 		MOV result1,#1  ;验证结果
-// One:	MOV A,#254
-// 	#pragma endasm
+// #pragma asm
+// 		MOV 30H,#56H
+// 		MOV 31H,#95H
+// 		MOV 32H,#54H
+// 		MOV 33H,#0E5H	;准备测试数据
+// 		MOV 40H,#0
+// 		MOV 41H,#0		;将结果空间值清0
+// 		MOV R0,#30H
+// 		MOV R1,#40H		;间址寄存器设置初值
+// LOOP:	MOV A,@R1
+// 		ADD A,@R0		
+// 		MOV @R1,A		;将R0和结果低位相加
+// 		INC R1
+// 		MOV A,@R1
+// 		ADDC A,#0
+// 		MOV @R1,A		
+// 		DEC R1			;将相加进位加入结果高位，并重置R1为40H
+// 		INC R0			;R0加1，为之后运算做准备
+// 	CJNE R0,#34H,LOOP	;循环跳转条件R0<#34H
+// 	;结果是224
 }
